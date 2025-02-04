ec857c5103ba3c9d38f4d199b0093358c3de9654
Ivaaan11 <ivanlobacev787@gmail.com>
2025-02-04 20:39:10 +0300

    database

--- src/app/database/models.py
+++ src/app/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import BigInteger
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
 engine = create_async_engine(url='postgresql+asyncpg://user:password@postgres:5432/db')
@@ -16,15 +16,19 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id = mapped_column(BigInteger)
-    username: Mapped[str] = mapped_column()
+    username: Mapped[str] = mapped_column(unique=True)
+
+    tickets: Mapped[list["Ticket"]] = relationship(back_populates="creator")
 
 
-class Admin(Base):
-    __tablename__ = 'admins'
+class Ticket(Base):
+    __tablename__ = 'tickets'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    tg_id = mapped_column(BigInteger)
+    text: Mapped[str] = mapped_column()
+    creator_username: Mapped[str] = mapped_column(ForeignKey('users.username'))
 
+    creator: Mapped["User"] = relationship(back_populates="tickets")
 
 
 async def async_main():
